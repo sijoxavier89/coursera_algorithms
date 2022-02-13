616cebd43f3c5964aaa46e9eaafbbf4693b1ea72
sijoxavier89 <sijo.xavier@yahoo.com>
2022-02-13 15:57:57 -0600

    initial commit for wordnet

---
+++ wordnet/Outcast.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name: Sijo Xavier
+ *  Date: Feb 13, 2022
+ *  Description: Outcast
+ * Given a list of WordNet nouns x1, x2, ..., xn, which noun is the least related to the others? To identify an outcast,
+ * compute the sum of the distances between each noun and every other one:
+ * di   =   distance(xi, x1)   +   distance(xi, x2)   +   ...   +   distance(xi, xn)
+ **************************************************************************** */
+
+public class Outcast {
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        return null;
+    }
+
+    // see test client below
+    public static void main(String[] args) {
+
+    }
+}
---
+++ wordnet/SAP.java
@@ -0,0 +1,43 @@
+/* *****************************************************************************
+ *  Name: Sijo Xavier
+ *  Date: 13 - Feb - 2022
+ *  Description:
+ * Performance requirements.  All methods (and the constructor) should take time at most proportional to E + V in the worst case,
+ *  where E and V are the number of edges and vertices in the digraph, respectively. Your data type should use space proportional to E + V.
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Digraph;
+
+public class SAP {
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        return 0;
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        return 0;
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        return 0;
+
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        return 0;
+
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+
+    }
+}
